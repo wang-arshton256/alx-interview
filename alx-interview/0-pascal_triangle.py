442d5b0bf7039bad871da8ad70d2a61e2c01352a
Wangutusi Arshton <wangarshton@gmail.com>
2024-06-18 20:51:13 +0300

    Pascal's Triangle

--- alx-interview/0-pascal_triangle.py
+++ alx-interview/0-pascal_triangle.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
-"""This module defines a function that creates a Pascal triangle
+""" pascal triangle written in python
 """
 
 
 def pascal_triangle(n):
-    """Return a Pascal's triangle
+    """ returns pascal triangle
     """
     if n <= 0:
         return []
 
-    # create list triangle
-    tri = [[1 for i in range(j + 1)] for j in range(n)]
+    triangle = []
 
-    for row_n, row in enumerate(tri):
-        for col in range(len(row) - 1):
-            if col is 0:  # skip [n][0] index
-                continue
-            tri[row_n][col] = tri[row_n - 1][col - 1] + tri[row_n - 1][col]
-    return tri
+    for i in range(n):
+        mylist = [1]
+        """if its the first digit then use"""
+        if i == 0:
+            """mylist as the first digit"""
+            triangle.append(mylist)
+            continue
+        else:
+            """get the last row"""
+            prev_row = triangle[-1]
+            for j in range(1, i):
+                nex = prev_row[j - 1] + prev_row[j]
+                mylist.append(nex)
+        """append 1 at the end of the list"""
+        mylist.append(1)
+        """the first digit should always be mylist"""
+        triangle.append(mylist)
+
+    return triangle
